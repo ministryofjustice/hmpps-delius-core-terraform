a865c0767e67b72a150ded240686cba3d037077f
smjmoj <smjmoj@gmail.com>
2018-07-27 10:46:34 +0100

    Additional ingress rules for weblogic servers

--- security-groups/weblogic-in.tf
+++ security-groups/weblogic-in.tf
@@ -3,7 +3,7 @@
 resource "aws_security_group" "weblogic_in" {
   name        = "${local.environment_name}-weblogic-in"
   vpc_id      = "${data.aws_vpc.vpc.id}"
-  description = "Weblogic master in"
+  description = "Weblogic servers ingress"
   tags        = "${merge(var.tags, map("Name", "${local.environment_name}_weblogic_in", "Type", "WLS"))}"
 
   lifecycle {
@@ -11,11 +11,38 @@ resource "aws_security_group" "weblogic_in" {
   }
 }
 
-resource "aws_security_group_rule" "weblogic-elb-in" {
+resource "aws_security_group_rule" "weblogic_elb_in" {
   security_group_id = "${aws_security_group.weblogic_in.id}"
   type              = "ingress"
   from_port         = 9704
   to_port           = 9704
   protocol          = "tcp"
   source_security_group_id = "${aws_security_group.weblogic_elb.id}"
+}
+
+resource "aws_security_group_rule" "weblogic_out_weblogic_in_7001_2" {
+  security_group_id = "${aws_security_group.weblogic_in.id}"
+  type              = "ingress"
+  from_port         = 7001
+  to_port           = 7002
+  protocol          = "tcp"
+  source_security_group_id = "${aws_security_group.weblogic_out.id}"
+}
+
+resource "aws_security_group_rule" "weblogic_out_weblogic_in_7005" {
+  security_group_id = "${aws_security_group.weblogic_in.id}"
+  type              = "ingress"
+  from_port         = 7005
+  to_port           = 7005
+  protocol          = "tcp"
+  source_security_group_id = "${aws_security_group.weblogic_out.id}"
+}
+
+resource "aws_security_group_rule" "db_out_weblogic_in" {
+  security_group_id = "${aws_security_group.weblogic_in.id}"
+  type              = "ingress"
+  from_port         = 61616
+  to_port           = 61617
+  protocol          = "tcp"
+  source_security_group_id = "${aws_security_group.db_out.id}"
 }
