a071715cc8959b1d49a953e956b9266af2ba2212
smjmoj <smjmoj@gmail.com>
2018-07-27 11:55:57 +0100

    These security groups are to be applied to the correct items in application tf subdir. Much simplified, each group (lb, weblogic & db) have an ingress and egress security group. This will be easier to update and understand until we're clear on the netowrking.

--- security-groups/db-out.tf
+++ security-groups/db-out.tf
@@ -1,3 +1,5 @@
+# db-out.tf
+
 resource "aws_security_group" "db_out" {
   name        = "${local.environment_name}-db-out"
   vpc_id      = "${data.aws_vpc.vpc.id}"
@@ -9,11 +11,12 @@ resource "aws_security_group" "db_out" {
   }
 }
 
-resource "aws_security_group_rule" "db_out" {
-  security_group_id = "${aws_security_group.db_out.id}"
-  type              = "egress"
-  from_port         = 1521
-  to_port           = 1521
-  protocol          = "tcp"
-  source_security_group_id = "${aws_security_group.db_in.id}"
+## egress rules
+resource "aws_security_group_rule" "db_out_weblogic_in" {
+  security_group_id         = "${aws_security_group.db_out.id}"
+  type                      = "egress"
+  from_port                 = 61616
+  to_port                   = 61617
+  protocol                  = "tcp"
+  source_security_group_id  = "${aws_security_group.weblogic_in.id}"
 }
--- security-groups/weblogic-in.tf
+++ security-groups/weblogic-in.tf
@@ -11,38 +11,48 @@ resource "aws_security_group" "weblogic_in" {
   }
 }
 
-resource "aws_security_group_rule" "weblogic_elb_in" {
-  security_group_id = "${aws_security_group.weblogic_in.id}"
-  type              = "ingress"
-  from_port         = 9704
-  to_port           = 9704
-  protocol          = "tcp"
-  source_security_group_id = "${aws_security_group.weblogic_elb.id}"
+## ingress rules
+resource "aws_security_group_rule" "weblogic_lb_out_weblogic_in" {
+  security_group_id         = "${aws_security_group.weblogic_in.id}"
+  type                      = "ingress"
+  from_port                 = 9704
+  to_port                   = 9704
+  protocol                  = "tcp"
+  source_security_group_id  = "${aws_security_group.weblogic_lb_out.id}"
 }
 
 resource "aws_security_group_rule" "weblogic_out_weblogic_in_7001_2" {
-  security_group_id = "${aws_security_group.weblogic_in.id}"
-  type              = "ingress"
-  from_port         = 7001
-  to_port           = 7002
-  protocol          = "tcp"
-  source_security_group_id = "${aws_security_group.weblogic_out.id}"
+  security_group_id         = "${aws_security_group.weblogic_in.id}"
+  type                      = "ingress"
+  from_port                 = 7001
+  to_port                   = 7002
+  protocol                  = "tcp"
+  source_security_group_id  = "${aws_security_group.weblogic_out.id}"
 }
 
 resource "aws_security_group_rule" "weblogic_out_weblogic_in_7005" {
-  security_group_id = "${aws_security_group.weblogic_in.id}"
-  type              = "ingress"
-  from_port         = 7005
-  to_port           = 7005
-  protocol          = "tcp"
-  source_security_group_id = "${aws_security_group.weblogic_out.id}"
+  security_group_id         = "${aws_security_group.weblogic_in.id}"
+  type                      = "ingress"
+  from_port                 = 7005
+  to_port                   = 7005
+  protocol                  = "tcp"
+  source_security_group_id  = "${aws_security_group.weblogic_out.id}"
+}
+
+resource "aws_security_group_rule" "weblogic_out_weblogic_in_3060" {
+  security_group_id         = "${aws_security_group.weblogic_in.id}"
+  type                      = "ingress"
+  from_port                 = 3060
+  to_port                   = 3060
+  protocol                  = "tcp"
+  source_security_group_id  = "${aws_security_group.weblogic_out.id}"
 }
 
 resource "aws_security_group_rule" "db_out_weblogic_in" {
-  security_group_id = "${aws_security_group.weblogic_in.id}"
-  type              = "ingress"
-  from_port         = 61616
-  to_port           = 61617
-  protocol          = "tcp"
-  source_security_group_id = "${aws_security_group.db_out.id}"
+  security_group_id         = "${aws_security_group.weblogic_in.id}"
+  type                      = "ingress"
+  from_port                 = 61616
+  to_port                   = 61617
+  protocol                  = "tcp"
+  source_security_group_id  = "${aws_security_group.db_out.id}"
 }
