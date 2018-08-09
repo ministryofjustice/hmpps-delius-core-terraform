4bc2589a50ec93e7907fb7edf43274e3d9453319
smjmoj <smjmoj@gmail.com>
2018-08-09 16:48:15 +0100

    Added outputs to enable quicker updating of the perring code in the other repo https://github.com/ministryofjustice/hmpps-delius-bastion env_configs/dev.tfvars

--- network/outputs.tf
+++ network/outputs.tf
@@ -1,3 +1,11 @@
+output "vpc_id" {
+  value = "${module.network.vpc_id}"
+}
+
+output "vpc_cidr_block" {
+  value = "${module.network.vpc_cidr_block}"
+}
+
 output "bastion_peering_id" {
   value = "${module.network.bastion_peering_id}"
 }
