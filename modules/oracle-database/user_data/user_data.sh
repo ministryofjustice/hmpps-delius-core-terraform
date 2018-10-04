f59ccf847e74e4466444c5d77dd4eaac6dcbd90b
Brett Minnie <github@bdb-studios.co.uk>
2018-10-04 10:42:23 +0100

    Updated oracledb bootstrap script to consume our galaxy role

--- modules/oracle-database/user_data/user_data.sh
+++ modules/oracle-database/user_data/user_data.sh
@@ -4,9 +4,13 @@ exec > >(tee /var/log/user-data.log|logger -t user-data ) 2>&1
 echo BEGIN
 date '+%Y-%m-%d %H:%M:%S'
 
+yum install -y wget git python-pip
+pip install -U pip
+pip install ansible
+
 cat << EOF >> /etc/environment
 HMPPS_ROLE="${app_name}"
-HMPPS_FQDN="`curl http://169.254.169.254/latest/meta-data/instance-id`.${private_domain}"
+HMPPS_FQDN="${app_name}.${private_domain}"
 HMPPS_STACKNAME=${env_identifier}
 HMPPS_STACK="${short_env_identifier}"
 HMPPS_ENVIRONMENT=${route53_sub_domain}
@@ -16,7 +20,7 @@ EOF
 ## Ansible runs in the same shell that has just set the env vars for future logins so it has no knowledge of the vars we've
 ## just configured, so lets export them
 export HMPPS_ROLE="${app_name}"
-export HMPPS_FQDN="`curl http://169.254.169.254/latest/meta-data/instance-id`.${private_domain}"
+export HMPPS_FQDN="${app_name}.${private_domain}"
 export HMPPS_STACKNAME="${env_identifier}"
 export HMPPS_STACK="${short_env_identifier}"
 export HMPPS_ENVIRONMENT=${route53_sub_domain}
@@ -29,12 +33,10 @@ cat << EOF > ~/requirements.yml
 - name: bootstrap
   src: https://github.com/ministryofjustice/hmpps-bootstrap
   version: centos
-# - name: rsyslog
-#   src: https://github.com/ministryofjustice/hmpps-rsyslog-role
-# - name: elasticbeats
-#   src: https://github.com/ministryofjustice/hmpps-beats-monitoring
 - name: users
   src: singleplatform-eng.users
+- name: oracle-db
+  src: https://github.com/ministryofjustice/hmpps-delius-core-oracledb-bootstrap.git
 EOF
 
 /usr/bin/curl -o ~/users.yml https://raw.githubusercontent.com/ministryofjustice/hmpps-delius-ansible/master/group_vars/bastion
@@ -42,6 +44,12 @@ EOF
 cat << EOF > ~/vars.yml
 region: "${region}" \
 
+# These values are to be updated when the are injected and pulled from paramstore, consumed by oradb bootstrap
+# oradb_sys_password            
+# oradb_system_password
+# oradb_sysman_password
+# oradb_dbsnmp_password
+# oradb_asmsnmp_password
 
 EOF
 cat << EOF > ~/bootstrap.yml
@@ -54,9 +62,8 @@ cat << EOF > ~/bootstrap.yml
   roles:
      - bootstrap
      - users
-     # - rsyslog
-     # - elasticbeats
+     - oracle-db
 EOF
 
 ansible-galaxy install -f -r ~/requirements.yml
-SELF_REGISTER=true ansible-playbook ~/bootstrap.yml
+CONFIGURE_SWAP=true SELF_REGISTER=true ansible-playbook ~/bootstrap.yml
