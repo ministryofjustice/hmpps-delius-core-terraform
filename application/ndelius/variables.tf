cf301bec3284165f923cbf409fc38e1282a70efe
smjmoj <smjmoj@gmail.com>
2018-11-26 13:13:28 +0000

    Added ansible apache ds vars to delius products

--- application/ndelius/variables.tf
+++ application/ndelius/variables.tf
@@ -51,3 +51,8 @@ variable "ansible_vars" {
   description = "Ansible vars for user_data script"
   type        = "map"
 }
+
+variable "ansible_vars_apacheds" {
+  description = "Ansible (ldap) vars for user_data script "
+  type        = "map"
+}
