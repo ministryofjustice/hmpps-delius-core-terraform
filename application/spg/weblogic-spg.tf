8dedb18eba95bafb958f3fdc610c58ed357b3427
marcus-bcl <marcus.aspin@bconline.co.uk>
2018-12-11 17:07:49 +0000

    Prefix log group names with environment id

--- application/spg/weblogic-spg.tf
+++ application/spg/weblogic-spg.tf
@@ -66,7 +66,7 @@ module "spg" {
   ndelius_version = "${var.ndelius_version}"
 
   ansible_vars = {
-    cldwatch_log_group       = "weblogic-spg"
+    cldwatch_log_group       = "${var.environment_identifier}/weblogic-spg"
     setup_datasources        = "${var.ansible_vars["setup_datasources"]}"
     s3_dependencies_bucket   = "${substr("${var.dependencies_bucket_arn}", 13, -1)}"
     database_host            = "${var.ansible_vars["database_host"]}.${data.aws_route53_zone.public.name}"
