c4d091412780a577f2f69f7f52857a4a92f7bf27
marcus.aspin <marcus.aspin@bconline.co.uk>
2018-12-20 16:06:52 +0000

    Point health-check at WL console until endpoint is available in delius

--- application/ndelius/weblogic-ndelius.tf
+++ application/ndelius/weblogic-ndelius.tf
@@ -44,7 +44,7 @@ module "ndelius" {
   internal_elb_sg_id           = "${data.terraform_remote_state.delius_core_security_groups.sg_weblogic_ndelius_internal_elb_id}"
   external_elb_sg_id           = "${data.terraform_remote_state.delius_core_security_groups.sg_weblogic_ndelius_external_elb_id}"
   // TODO: update when health check endpoint is available in delius:
-  weblogic_health_check_path   = "console/login/LoginForm.jsp/"
+  weblogic_health_check_path   = "console/login/LoginForm.jsp"
   weblogic_port                = "${var.weblogic_domain_ports["weblogic_port"]}"
   weblogic_tls_port            = "${var.weblogic_domain_ports["weblogic_tls_port"]}"
   activemq_port                = "${var.weblogic_domain_ports["activemq_port"]}"
