309c1327ce3c026d2c2567ecc1392a13dd9f632a
marcus.aspin <marcus.aspin@bconline.co.uk>
2019-01-09 22:15:51 +0000

    Added slave node to ldap module

--- modules/apacheds-ldap/ldap.tf
+++ modules/apacheds-ldap/ldap.tf
@@ -23,6 +23,29 @@ resource "aws_instance" "ldap" {
   }
 }
 
+resource "aws_instance" "ldap_slave" {
+  ami                    = "${var.ami_id}"
+  instance_type          = "${var.instance_type}"
+  key_name               = "${var.key_name}"
+  iam_instance_profile   = "${var.iam_instance_profile}"
+  vpc_security_group_ids = ["${var.security_groups}"]
+  subnet_id              = "${var.private_subnets[0]}"
+  user_data              = "${data.template_file.user_data_slave.rendered}"
+  source_dest_check      = false
+
+  root_block_device = {
+    delete_on_termination = true
+    volume_size           = 50
+    volume_type           = "gp2"
+  }
+
+  tags = "${merge(var.tags, map("Name", "${var.environment_name}-${var.tier_name}-ldap-slave"))}"
+
+  lifecycle {
+    ignore_changes = ["ami", "user_data"]
+  }
+}
+
 resource "aws_route53_record" "ldap_instance_internal" {
   zone_id = "${var.private_zone_id}"
   name    = "${var.tier_name}-instance"
--- modules/apacheds-ldap/main.tf
+++ modules/apacheds-ldap/main.tf
@@ -31,7 +31,42 @@ data "template_file" "user_data" {
     partition_id           = "${var.ansible_vars["partition_id"]}"
     import_users_ldif      = "${var.ansible_vars["import_users_ldif"]}"
     sanitize_oid_ldif      = "${var.ansible_vars["sanitize_oid_ldif"]}"
-    is_consumer            = "${var.ansible_vars["is_consumer"]}"
-    provider_host          = "${var.ansible_vars["provider_host"]}"
+    is_consumer            = "false"
+  }
+}
+
+data "template_file" "user_data_slave" {
+  template = "${file("${path.module}/user_data/user_data.sh")}"
+
+  vars {
+    env_identifier                = "${var.environment_identifier}"
+    short_env_identifier          = "${var.short_environment_identifier}"
+    region                        = "${var.region}"
+    app_name                      = "${var.tier_name}"
+    route53_sub_domain            = "${var.environment_name}"
+    environment_name              = "${var.environment_name}"
+    private_domain                = "${var.private_domain}"
+    account_id                    = "${var.vpc_account_id}"
+    bastion_inventory             = "${var.bastion_inventory}"
+    app_bootstrap_name            = "${var.app_bootstrap_name}"
+    app_bootstrap_src             = "${var.app_bootstrap_src}"
+    app_bootstrap_version         = "${var.app_bootstrap_version}"
+    app_bootstrap_initial_role    = "${var.app_bootstrap_initial_role}"
+    app_bootstrap_secondary_role  = "${var.app_bootstrap_secondary_role}"
+    app_bootstrap_tertiary_role   = "${var.app_bootstrap_tertiary_role}"
+
+    ndelius_version        = "${var.ndelius_version}"
+    cldwatch_log_group     = "${var.ansible_vars["cldwatch_log_group"]}"
+    s3_dependencies_bucket = "${var.ansible_vars["s3_dependencies_bucket"]}"
+    apacheds_version       = "${var.ansible_vars["apacheds_version"]}"
+    ldap_protocol          = "${var.ansible_vars["ldap_protocol"]}"
+    ldap_port              = "${var.ansible_vars["ldap_port"]}"
+    bind_user              = "${var.ansible_vars["bind_user"]}"
+    # bind_password        = "/TG_ENVIRONMENT_NAME/TG_PROJECT_NAME/apacheds/apacheds/ldap_admin_password"
+    partition_id           = "${var.ansible_vars["partition_id"]}"
+    import_users_ldif      = "${var.ansible_vars["import_users_ldif"]}"
+    sanitize_oid_ldif      = "${var.ansible_vars["sanitize_oid_ldif"]}"
+    is_consumer            = "true"
+    provider_host          = "${aws_route53_record.ldap_instance_internal.fqdn}"
   }
 }
