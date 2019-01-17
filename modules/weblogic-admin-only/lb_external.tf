850145fd759612f198d381e08174c6d6e9a142ef
marcus.aspin <marcus.aspin@bconline.co.uk>
2019-01-17 13:42:33 +0000

    Updated default action on HTTP LB to redirect to HTTPS LB

--- modules/weblogic-admin-only/lb_external.tf
+++ modules/weblogic-admin-only/lb_external.tf
@@ -48,8 +48,12 @@ resource "aws_lb_listener" "external_lb_listener_insecure" {
   protocol            = "HTTP"
   port                = "80"
   default_action {
-    target_group_arn  = "${module.external_lb_target_group.target_group_arn}"
-    type              = "forward"
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
 }
 
@@ -58,22 +62,6 @@ resource "aws_app_cookie_stickiness_policy" "external_lb_jsessionid_stickiness_p
   load_balancer = "${aws_lb.external_lb.name}"
   lb_port       = 443
   cookie_name   = "JSESSIONID"
-}
-
-resource "aws_lb_listener_rule" "external_lb_redirect_http_to_https" {
-  listener_arn = "${aws_lb_listener.external_lb_listener_insecure.arn}"
-  action {
-    type = "redirect"
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-  condition {
-    field  = "path-pattern"
-    values = ["/*"]
-  }
 }
 
 resource "aws_route53_record" "external_lb_private" {
