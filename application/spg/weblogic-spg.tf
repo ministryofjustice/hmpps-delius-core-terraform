59e8e0d9930c63d799aa9a58aaf9fae8382c12bd
smjmoj <smjmoj@gmail.com>
2019-06-04 11:41:38 +0100

    Enable instances to update their own tags in ASG when bootstrapping and version deployed. Primarily to enable ndelius version in tag.

--- application/spg/weblogic-spg.tf
+++ application/spg/weblogic-spg.tf
@@ -44,7 +44,7 @@ module "spg" {
     data.terraform_remote_state.vpc.vpc_private-subnet-az3,
   )}"
 
-  tags                         = "${var.tags}"
+  tags                         = "${merge(var.tags, map("ndelius_version", "None deployed"))}"
   environment_name             = "${data.terraform_remote_state.vpc.environment_name}"
   bastion_inventory            = "${data.terraform_remote_state.vpc.bastion_inventory}"
   project_name                 = "${var.project_name}"
