a77b2e83988fb2b829bd9284186f6a7b3e66e58b
marcus.aspin <marcus.aspin@bconline.co.uk>
2019-06-10 10:53:36 +0100

    Add role, instance_id and region to env data

--- management/user_data/user_data.sh
+++ management/user_data/user_data.sh
@@ -5,26 +5,31 @@ echo BEGIN
 date '+%Y-%m-%d %H:%M:%S'
 
 yum install -y wget git python-pip openldap-clients
-
 pip install -U pip
 pip install ansible ansible==2.6
 
 cat << EOF >> /etc/environment
-HMPPS_FQDN="`curl http://169.254.169.254/latest/meta-data/instance-id`.${private_domain}"
-HMPPS_STACKNAME=${env_identifier}
-HMPPS_STACK="${short_env_identifier}"
-HMPPS_ENVIRONMENT=${environment_name}
-HMPPS_ACCOUNT_ID="${account_id}"
-HMPPS_DOMAIN="${private_domain}"
-EOF
-## Ansible runs in the same shell that has just set the env vars for future logins so it has no knowledge of the vars we've
-## just configured, so lets export them
+export HMPPS_ROLE="management-server"
 export HMPPS_FQDN="`curl http://169.254.169.254/latest/meta-data/instance-id`.${private_domain}"
 export HMPPS_STACKNAME="${env_identifier}"
 export HMPPS_STACK="${short_env_identifier}"
-export HMPPS_ENVIRONMENT=${environment_name}
+export HMPPS_ENVIRONMENT="${environment_name}"
 export HMPPS_ACCOUNT_ID="${account_id}"
 export HMPPS_DOMAIN="${private_domain}"
+export INSTANCE_ID="`curl http://169.254.169.254/latest/meta-data/instance-id`"
+export REGION="${region}"
+EOF
+## Ansible runs in the same shell that has just set the env vars for future logins so it has no knowledge of the vars we've
+## just configured, so lets export them
+export HMPPS_ROLE="${app_name}"
+export HMPPS_FQDN="`curl http://169.254.169.254/latest/meta-data/instance-id`.${private_domain}"
+export HMPPS_STACKNAME="${env_identifier}"
+export HMPPS_STACK="${short_env_identifier}"
+export HMPPS_ENVIRONMENT="${environment_name}"
+export HMPPS_ACCOUNT_ID="${account_id}"
+export HMPPS_DOMAIN="${private_domain}"
+export INSTANCE_ID="`curl http://169.254.169.254/latest/meta-data/instance-id`"
+export REGION="${region}"
 
 cat << EOF > ~/requirements.yml
 ---
