774afc752e5d6f89b7b032d6aa0bd73bbeafe4f6
marcus.aspin <maspin@unilink.com>
2019-12-30 16:46:00 +0000

    Added alarms for inbound queue size

--- monitoring/cloudwatch.tf
+++ monitoring/cloudwatch.tf
@@ -104,3 +104,39 @@ resource "aws_cloudwatch_metric_alarm" "activemq_healthy_hosts_fatal_alarm" {
     LoadBalancerName = "${data.terraform_remote_state.spg.jms_lb["name"]}"
   }
 }
+
+resource "aws_cloudwatch_metric_alarm" "inbound_queue_size_warning_alarm" {
+  alarm_name                = "${var.environment_name}-activemq-inbound-queue-size-cwa--warning"
+  alarm_description         = "Inbound queue size exceeded 25 for 30 minutes."
+  namespace                 = "WebLogic"
+  statistic                 = "Sum"
+  metric_name               = "InboundQueueSize"
+  comparison_operator       = "GreaterThanThreshold"
+  threshold                 = "25"
+  evaluation_periods        = "30"
+  period                    = "60"
+  alarm_actions             = ["${aws_sns_topic.alarm_notification.arn}"]
+  ok_actions                = ["${aws_sns_topic.alarm_notification.arn}"]
+  insufficient_data_actions = ["${aws_sns_topic.alarm_notification.arn}"]
+  dimensions {
+    AutoScalingGroupName = "${data.terraform_remote_state.spg.asg["name"]}"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "inbound_queue_size_critical_alarm" {
+  alarm_name                = "${var.environment_name}-activemq-inbound-queue-size-cwa--critical"
+  alarm_description         = "Inbound queue size exceeded 100 for 3 hours."
+  namespace                 = "WebLogic"
+  statistic                 = "Sum"
+  metric_name               = "InboundQueueSize"
+  comparison_operator       = "GreaterThanThreshold"
+  threshold                 = "100"
+  evaluation_periods        = "180"
+  period                    = "60"
+  alarm_actions             = ["${aws_sns_topic.alarm_notification.arn}"]
+  ok_actions                = ["${aws_sns_topic.alarm_notification.arn}"]
+  insufficient_data_actions = ["${aws_sns_topic.alarm_notification.arn}"]
+  dimensions {
+    AutoScalingGroupName = "${data.terraform_remote_state.spg.asg["name"]}"
+  }
+}
