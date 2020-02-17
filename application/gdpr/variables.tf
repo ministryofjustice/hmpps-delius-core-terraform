1278927aef6927a8edee87f62670fafb636f7173
marcus.aspin <maspin@unilink.com>
2020-02-17 12:25:07 +0000

    Updated to match prod config and set log level to INFO

--- application/gdpr/variables.tf
+++ application/gdpr/variables.tf
@@ -26,13 +26,13 @@ variable "gdpr_config" {
 }
 
 variable "default_gdpr_config" {
-  description = "Default values to be overridden by gdpr_config"
+  description = "Default values to be overridden by gdpr_config. This should match the intended config for production."
   type = "map"
   default = {
     api_image_url               = "895523100917.dkr.ecr.eu-west-2.amazonaws.com/hmpps/delius-gdpr"
-    api_version                 = "0.9"                  # Application version
-    api_memory                  = 2048                   # Memory to assign to API container
-    api_cpu                     = 1024                   # CPU to assign to API container
+    api_version                 = "0.11"                 # Application version
+    api_memory                  = 4196                   # Memory to assign to API container
+    api_cpu                     = 2048                   # CPU to assign to API container
     cron_identifyduplicates     = "-"                    # Batch schedules. Set to "-" to disable.
     cron_retainedoffenders      = "-"                    #
     cron_retainedoffendersiicsa = "-"                    #
@@ -40,20 +40,19 @@ variable "default_gdpr_config" {
     cron_deleteoffenders        = "-"                    #
     cron_destructionlogclearing = "-"                    #
     ui_image_url                = "895523100917.dkr.ecr.eu-west-2.amazonaws.com/hmpps/delius-gdpr-ui"
-    ui_version                  = "0.9"                  # Application version
-    ui_memory                   = 2048                   # Memory to assign to UI container
+    ui_version                  = "0.11"                 # Application version
+    ui_memory                   = 1024                   # Memory to assign to UI container
     ui_cpu                      = 1024                   # CPU to assign to UI container
-    db_instance_class           = "db.t3.small"          # Instance type to use for the database
-    db_storage                  = 30                     # Allocated database storage in GB
+    db_instance_class           = "db.m5.large"          # Instance type to use for the database
+    db_storage                  = 100                    # Allocated database storage in GB
     db_maintenance_window       = "Wed:21:00-Wed:23:00"  # Maintenance window for database patching/upgrades
     db_backup_window            = "19:00-21:00"          # Daily window to take RDS backups
-    db_backup_retention_period  = 7                      # Number of days to retain RDS backups for
+    db_backup_retention_period  = 14                     # Number of days to retain RDS backups for
     scaling_min_capacity        = 2                      # Minimum number of running tasks per service
     scaling_max_capacity        = 10                     # Maximum number of running tasks per service
     target_cpu                  = 60                     # CPU target value for scaling of ECS tasks
-    log_level                   = "DEBUG"                # Application log-level
+    log_level                   = "INFO"                 # Application log-level
   }
-}
 
 variable "tags" {
   description = "Tags to be applied to resources"
