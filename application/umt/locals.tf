301278744ce469f1f968a481d58ba0e1b997e31d
marcus.aspin <maspin@unilink.com>
2020-04-06 19:02:40 +0100

    Added Delius LDAP bases

--- application/umt/locals.tf
+++ application/umt/locals.tf
@@ -2,5 +2,6 @@ locals {
   app_name      = "usermanagement"
   image_url     = "895523100917.dkr.ecr.eu-west-2.amazonaws.com/hmpps/ndelius-um"
   umt_config    = "${merge(var.default_umt_config, var.umt_config)}"
+  ldap_config   = "${merge(var.default_ldap_config, var.ldap_config)}"
   ansible_vars  = "${merge(var.default_ansible_vars, var.ansible_vars)}"
 }
