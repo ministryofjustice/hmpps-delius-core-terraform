2b082d716dcfc790a5f7d951d024710033b12623
Marcus Aspin <maspin@unilink.com>
2020-07-14 10:58:36 +0100

    Added UMT auth security group + increased healthcheck grace period (#361)

--- application/aptracker-api/ecs.tf
+++ application/aptracker-api/ecs.tf
@@ -4,17 +4,18 @@ module "ecs_service" {
   short_environment_name = "${var.short_environment_name}"
   tags                   = "${var.tags}"
 
-  service_name         = "${local.app_name}"
-  container_definition = "${data.template_file.container_definition.rendered}"
-  required_cpu         = "${local.aptracker_api_config["cpu"]}"
-  required_memory      = "${local.aptracker_api_config["memory"]}"
-  min_capacity         = "${local.aptracker_api_config["ecs_scaling_min_capacity"]}"
-  max_capacity         = "${local.aptracker_api_config["ecs_scaling_max_capacity"]}"
-  target_cpu_usage     = "${local.aptracker_api_config["ecs_target_cpu"]}"
-  vpc_id               = "${data.terraform_remote_state.vpc.vpc_id}"
-  lb_listener_arn      = "${data.terraform_remote_state.ndelius.lb_listener_arn}"
-  lb_path_patterns     = ["/aptracker-api", "/aptracker-api/*"]
-  health_check_path    = "/aptracker-api/actuator/health"
+  service_name                      = "${local.app_name}"
+  container_definition              = "${data.template_file.container_definition.rendered}"
+  required_cpu                      = "${local.aptracker_api_config["cpu"]}"
+  required_memory                   = "${local.aptracker_api_config["memory"]}"
+  min_capacity                      = "${local.aptracker_api_config["ecs_scaling_min_capacity"]}"
+  max_capacity                      = "${local.aptracker_api_config["ecs_scaling_max_capacity"]}"
+  target_cpu_usage                  = "${local.aptracker_api_config["ecs_target_cpu"]}"
+  vpc_id                            = "${data.terraform_remote_state.vpc.vpc_id}"
+  lb_listener_arn                   = "${data.terraform_remote_state.ndelius.lb_listener_arn}"
+  lb_path_patterns                  = ["/aptracker-api", "/aptracker-api/*"]
+  health_check_path                 = "/aptracker-api/actuator/health"
+  health_check_grace_period_seconds = 180
 
   ecs_cluster = {
     name         = "${data.terraform_remote_state.ecs_cluster.shared_ecs_cluster_name}"
@@ -29,7 +30,9 @@ module "ecs_service" {
   ]
 
   security_groups = [
+    "${data.terraform_remote_state.delius_core_security_groups.sg_common_out_id}",
     "${data.terraform_remote_state.delius_core_security_groups.sg_aptracker_api_id}",
+    "${data.terraform_remote_state.delius_core_security_groups.sg_umt_auth_id}",
   ]
 
   allowed_ssm_parameters = [
