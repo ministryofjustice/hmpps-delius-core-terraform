b8512980a43d901b15f3ff342fca2d9cb3d3dc02
Chris Kinsella <chris.kinsella@helkinsoft.com>
2020-09-24 17:14:45 +0100

    added test-instance code

--- batch/dss/data.tf
+++ batch/dss/data.tf
@@ -23,4 +23,52 @@ data "terraform_remote_state" "delius_core_security_groups" {
     region = var.region
   }
 }
+#-------------------------------------------------------------
+### Getting the shared vpc security groups
+#-------------------------------------------------------------
+data "terraform_remote_state" "vpc_security_groups" {
+  backend = "s3"
 
+  config = {
+    bucket = var.remote_state_bucket_name
+    key    = "security-groups/terraform.tfstate"
+    region = var.region
+  }
+}
+
+#-------------------------------------------------------------
+### Getting the sub project keys and profiles
+#-------------------------------------------------------------
+data "terraform_remote_state" "key_profile" {
+  backend = "s3"
+
+  config = {
+    bucket = var.remote_state_bucket_name
+    key    = "delius-core/key_profile/terraform.tfstate"
+    region = var.region
+  }
+}
+
+
+#-------------------------------------------------------------
+### test instance AMI
+#-------------------------------------------------------------
+data "aws_ami" "amazon_ami" {
+  owners      = ["895523100917"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["HMPPS Base Amazon Linux 2 LTS master *"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
