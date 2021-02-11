22e2b0a9d2cab3c22bfd7e2106caa1f487fb0615
marcus.aspin <maspin@unilink.com>
2021-02-11 13:25:13 +0000

    Dynamically ignore changes to the task definition  for externally-managed deployments

--- application/delius-api/ssm.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_ssm_parameter" "image_version" {
-  name        = "/versions/delius-core/ecs/delius-api/${var.environment_name}"
-  value       = "latest"
-  type        = "String"
-  description = "Delius-API image version. Managed by CircleCI."
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
