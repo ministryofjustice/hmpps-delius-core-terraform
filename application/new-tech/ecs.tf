677d2ddc892390aa2589766ee978184f7da35c38
marcus.aspin <maspin@unilink.com>
2021-03-11 16:57:51 +0000

    Updated some comments

--- application/new-tech/ecs.tf
+++ application/new-tech/ecs.tf
@@ -5,7 +5,9 @@ module "ecs" {
   tags                   = var.tags
 
   service_name = local.app_name
-  vpc_id       = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  # Security/Networking
+  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
   subnets = [
     data.terraform_remote_state.vpc.outputs.vpc_private-subnet-az1,
     data.terraform_remote_state.vpc.outputs.vpc_private-subnet-az2,
@@ -22,24 +24,26 @@ module "ecs" {
     namespace_id = data.terraform_remote_state.ecs_cluster.outputs.private_cluster_namespace["id"]
   }
 
-  required_cpu                   = local.app_config["cpu"]
-  required_memory                = local.app_config["memory"]
-  min_capacity                   = local.app_config["min_capacity"]
-  max_capacity                   = local.app_config["max_capacity"]
-  target_cpu_usage               = local.app_config["target_cpu"]
+  # Auto-scaling
+  required_cpu     = local.app_config["cpu"]
+  required_memory  = local.app_config["memory"]
+  min_capacity     = local.app_config["min_capacity"]
+  max_capacity     = local.app_config["max_capacity"]
+  target_cpu_usage = local.app_config["target_cpu"]
+
+  # Application-specific configuration
   service_port                   = 9000
   lb_listener_arn                = data.terraform_remote_state.ndelius.outputs.lb_listener_arn
   lb_path_patterns               = ["/newTech", "/newTech/*"]
   health_check_path              = "/newTech/healthcheck"
   ignore_task_definition_changes = true # Deployment managed by CircleCI
-
   container_definition = jsonencode([{
     essential    = true
     name         = local.app_name
     image        = local.app_config["image_url"]
     cpu          = tonumber(local.app_config["cpu"])
     memory       = tonumber(local.app_config["memory"])
-    portMappings = [{ containerPort = 9000 }]
+    portMappings = [{ hostPort = 9000, containerPort = 9000 }]
     environment  = [for key, value in local.environment : { name = key, value = value }]
     secrets      = [for key, value in local.secrets : { name = key, valueFrom = "arn:aws:ssm:${var.region}:${data.aws_caller_identity.current.account_id}:parameter${value}" }]
     logConfiguration = {
--- application/new-tech/locals.tf
+++ application/new-tech/locals.tf
@@ -6,9 +6,10 @@ locals {
     CUSTODY_API_USERNAME = "/${var.environment_name}/${var.project_name}/newtech/web/custody_api_username"
     CUSTODY_API_PASSWORD = "/${var.environment_name}/${var.project_name}/newtech/web/custody_api_password"
     GOOGLE_ANALYTICS_ID  = "/${var.environment_name}/${var.project_name}/monitoring/analytics/google_id"
+    # ... Add any other secrets here that should be pulled from AWS Systems Manager Parameter Store
   })
   environment = merge({ for key, value in local.app_config : replace(key, "env_", "") => value if length(regexall("^env_", key)) > 0 }, {
-    LDAP_STRING_FORMAT = "cn=%s,${data.terraform_remote_state.ldap.outputs.ldap_base_users}" # TODO check if this is still needed
+    LDAP_STRING_FORMAT = "cn=%s,${data.terraform_remote_state.ldap.outputs.ldap_base_users}"
     STORE_ALFRESCO_URL = "https://alfresco.${data.terraform_remote_state.vpc.outputs.public_zone_name}/alfresco/service/"
     # ... Add any other environment variables here that should be pulled from Terraform data sources
   })
