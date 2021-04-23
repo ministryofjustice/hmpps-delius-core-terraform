169da27317e74f23d944b2998e26e24cfc1793b1
pmiller <paul.miller@bconline.co.uk>
2021-04-23 13:09:30 +0100

    Removing duplicate merge_api_db_in

--- security-groups/delius-db-in.tf
+++ security-groups/delius-db-in.tf
@@ -198,17 +198,6 @@ resource "aws_security_group_rule" "delius_api_db_in" {
   description              = "Delius API In"
 }
 
-# Allow Merge API in
-resource "aws_security_group_rule" "merge_api_db_in" {
-  security_group_id        = aws_security_group.delius_db_in.id
-  type                     = "ingress"
-  protocol                 = "tcp"
-  from_port                = "1521"
-  to_port                  = "1521"
-  source_security_group_id = aws_security_group.merge_api.id
-  description              = "Merge API In"
-}
-
 # Allow CI (Jenkins/AWS CodePipeline) access to DB
 resource "aws_security_group_rule" "eng_ci_db_in_1521" {
   count                    = var.ci_db_ingress_1521 ? 1 : 0
