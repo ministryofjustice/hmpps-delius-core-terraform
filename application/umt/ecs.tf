7af778b37cb78123d85b53c7b0543b04a07b5a28
marcus.aspin <maspin@unilink.com>
2021-07-14 09:42:41 +0100

    ALS-3926 Bind as root user in UMT, to bypass limit of 500 results per query

--- application/umt/ecs.tf
+++ application/umt/ecs.tf
@@ -21,7 +21,7 @@ module "ecs" {
     SPRING_JPA_HIBERNATE_DDL-AUTO           = "none"
     SPRING_LDAP_URLS                        = "${data.terraform_remote_state.ldap.outputs.ldap_protocol}://${data.terraform_remote_state.ldap.outputs.private_fqdn_ldap_elb}:${data.terraform_remote_state.ldap.outputs.ldap_port}"
     SPRING_LDAP_EXPORT_USERNAME             = "cn=root,${local.ldap_config["base_root"]}"
-    SPRING_LDAP_USERNAME                    = local.ldap_config["bind_user"]
+    SPRING_LDAP_USERNAME                    = "cn=root,${local.ldap_config["base_root"]}"
     SPRING_LDAP_BASE                        = local.ldap_config["base_root"]
     SPRING_LDAP_USEORACLEATTRIBUTES         = "false"
     SPRING_REDIS_HOST                       = aws_route53_record.token_store_private_dns.fqdn
