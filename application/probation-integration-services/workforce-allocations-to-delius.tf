36ff3ff00a08af083dd741c84d7130738de215ae
Marcus Aspin <maspin@unilink.com>
2022-08-03 17:12:27 +0100

    PI-351 Add workforce-allocations-to-delius service (#529)

--- application/probation-integration-services/workforce-allocations-to-delius.tf
+++ application/probation-integration-services/workforce-allocations-to-delius.tf
@@ -1,4 +1,4 @@
-module "housekeeping" {
+module "workforce-allocations-to-delius" {
   source                   = "../../modules/ecs_service"
   region                   = var.region
   environment_name         = var.environment_name
@@ -7,27 +7,26 @@ module "housekeeping" {
   tags                     = var.tags
 
   # Application Container
-  service_name                   = "integration-housekeeping"
+  service_name                   = "workforce-allocations-to-delius"
   ignore_task_definition_changes = true
 
   # Security & Networking
   task_role_arn      = aws_iam_role.ecs_sqs_task.arn
   target_group_count = 0 # no load balancer required
   security_groups = [
-    data.terraform_remote_state.delius_core_security_groups.outputs.sg_common_out_id
+    data.terraform_remote_state.delius_core_security_groups.outputs.sg_common_out_id,
+    data.terraform_remote_state.delius_core_security_groups.outputs.sg_delius_db_access_id
   ]
 
   # Monitoring
   notification_arn = data.terraform_remote_state.alerts.outputs.aws_sns_topic_alarm_notification_arn
 
   # Scaling
-  cpu          = 128
-  memory       = 128
-  min_capacity = 1
-  max_capacity = 1
+  min_capacity = local.min_capacity
+  max_capacity = local.max_capacity
 }
 
-resource "aws_iam_role_policy_attachment" "housekeeping" {
-  role       = module.housekeeping.exec_role.name
+resource "aws_iam_role_policy_attachment" "workforce-allocations-to-delius" {
+  role       = module.workforce-allocations-to-delius.exec_role.name
   policy_arn = aws_iam_policy.access_ssm_parameters.arn
 }
