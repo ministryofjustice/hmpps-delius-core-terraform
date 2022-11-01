240e57f194f56821c32751a9db33ad8597195b1d
anthony-britton-moj <anthony-britton-moj@users.noreply.github.com>
2022-11-01 14:31:06 +0000

    Create ECS service for probation-offender-events

---
+++ application/probation-integration-services/probation-offender-events.tf
@@ -0,0 +1,32 @@
+module "probation-offender-events" {
+  source                   = "../../modules/ecs_service"
+  region                   = var.region
+  environment_name         = var.environment_name
+  short_environment_name   = var.short_environment_name
+  remote_state_bucket_name = var.remote_state_bucket_name
+  tags                     = var.tags
+
+  # Application Container
+  service_name                   = "probation-offender-events"
+  ignore_task_definition_changes = true
+
+  # Security & Networking
+  task_role_arn      = aws_iam_role.ecs_sqs_task.arn
+  target_group_count = 0 # no load balancer required
+  security_groups = [
+    data.terraform_remote_state.delius_core_security_groups.outputs.sg_common_out_id,
+    data.terraform_remote_state.delius_core_security_groups.outputs.sg_delius_db_access_id
+  ]
+
+  # Monitoring
+  notification_arn = data.terraform_remote_state.alerts.outputs.aws_sns_topic_alarm_notification_arn
+
+  # Scaling
+  min_capacity = local.min_capacity
+  max_capacity = local.max_capacity
+}
+
+resource "aws_iam_role_policy_attachment" "probation-offender-events" {
+  role       = module.probation-offender-events.exec_role.name
+  policy_arn = aws_iam_policy.access_ssm_parameters.arn
+}
