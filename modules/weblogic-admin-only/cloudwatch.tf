712eaa14037c1a4811e8b90d93d33d1ef18743d7
Marcus Aspin <maspin@unilink.com>
2022-11-07 16:21:25 +0000

    Add WebLogic maximum CPU alert and remove APIERR001 alert (#559)
    * Remove Slack alert for APIERR001
    
    * Add WebLogic maximum CPU alarm, to catch stuck threads on a single instance
    
    * Fix namespace
    
    * Enable OASYSERR006 in prod
    
    Co-authored-by: Paul Miller <pmiller@unilink.com>

--- modules/weblogic-admin-only/cloudwatch.tf
+++ modules/weblogic-admin-only/cloudwatch.tf
@@ -1,7 +1,6 @@
 locals {
   alert_on_errors = toset(compact([
-    "APIERR001", // Failure to communicate with Delius API
-    var.environment_name != "delius-prod" ? "OASYSERR006" : null, // Invalid character in OASYS message (disabled in prod)
+    "OASYSERR006", // Invalid character in OASYS message
   ]))
 }
 
@@ -21,7 +20,7 @@ resource "aws_cloudwatch_log_metric_filter" "log_error_filter" {
 
 resource "aws_cloudwatch_metric_alarm" "log_error_alarm" {
   for_each            = local.alert_on_errors
-  alarm_name          = "${var.environment_name}-${var.app_name}-logged-errors-${each.value}-cwa--critical"
+  alarm_name          = "${var.environment_name}-${var.app_name}-logged-errors-${each.value}-cwa--warning"
   alarm_description   = "`${each.value}` errors were detected in the `${var.app_name}` logs."
   namespace           = aws_cloudwatch_log_metric_filter.log_error_filter[each.value].metric_transformation.0.namespace
   metric_name         = aws_cloudwatch_log_metric_filter.log_error_filter[each.value].metric_transformation.0.name
@@ -31,4 +30,22 @@ resource "aws_cloudwatch_metric_alarm" "log_error_alarm" {
   evaluation_periods  = 1
   period              = 300
   alarm_actions       = [data.terraform_remote_state.alerts.outputs.aws_sns_topic_alarm_notification_arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "cpu_max_util_warning_alarm" {
+  alarm_name          = "${var.environment_name}-${var.app_name}-weblogic-max-cpu-cwa--critical"
+  alarm_description   = "A WebLogic instance's CPU utilization reached 100%."
+  namespace           = "ECS/ContainerInsights"
+  statistic           = "Maximum"
+  metric_name         = "CpuUtilized"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = 1000
+  evaluation_periods  = 3
+  period              = 300
+  alarm_actions       = [data.terraform_remote_state.alerts.outputs.aws_sns_topic_alarm_notification_arn]
+  ok_actions          = [data.terraform_remote_state.alerts.outputs.aws_sns_topic_alarm_notification_arn]
+  dimensions = {
+    ServiceName = module.ecs.service.name
+    ClusterName = module.ecs.cluster.name
+  }
 }
