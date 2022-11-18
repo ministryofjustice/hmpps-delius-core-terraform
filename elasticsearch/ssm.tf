3ac4eb000739a81b964bb9e7a219d4927ec28abe
bill-buchan <bill.buchan@digital.justice.gov.uk>
2022-11-18 18:09:46 +0000

    Create password parameter

--- elasticsearch/ssm.tf
+++ elasticsearch/ssm.tf
@@ -25,3 +25,32 @@ resource "aws_ssm_parameter" "endpoint" {
   value = aws_elasticsearch_domain.contact_search.endpoint
   type  = "String"
 }
+
+resource "random_string" "password_prefix" {
+  length    = 4
+  upper     = true
+  min_upper = 1
+  lower     = true
+  min_lower = 1
+  numeric   = false
+  special   = false
+}
+
+# random strings for Password policy
+resource "random_string" "password_remainder" {
+  length           = 50
+  special          = true
+  override_special = "!$%&*()-_=+[]{}<>:?"
+}
+
+resource "aws_ssm_parameter" "database_username" {
+  name  = "/${var.environment_name}/${var.project_name}/elasticsearch/${local.contact_search_name}/database_username"
+  value = "contact_search_pool"
+  type  = "String"
+}
+
+resource "aws_ssm_parameter" "database_password" {
+  name  = "/${var.environment_name}/${var.project_name}/elasticsearch/${local.contact_search_name}/database_password"
+  value = "${random_string.password_prefix.result}${substr(bcrypt(random_string.password_remainder.result), 4, 11)}"
+  type  = "SecureString"
+}
