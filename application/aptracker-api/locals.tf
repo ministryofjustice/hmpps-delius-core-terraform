af300dfe80aa9fbd3de10370946a24afc6edb98a
Marcus Aspin <maspin@unilink.com>
2022-12-29 10:24:52 +0000

    DST-13466 Remove Terraform for Approved Premises Tracker API service (#579)

--- application/aptracker-api/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  app_name   = "delius-aptracker-api"
-  app_config = merge(var.default_aptracker_api_config, var.aptracker_api_config)
-}
-
--- security-groups/weblogic-app.tf
+++ security-groups/weblogic-app.tf
@@ -43,13 +43,12 @@ resource "aws_security_group_rule" "ndelius_lb_egress_to_instances" {
 
 resource "aws_security_group_rule" "ndelius_lb_egress_to_services" {
   for_each = {
-    "User Management"               = [aws_security_group.umt_instances.id, 8080]
-    "New Tech Web"                  = [aws_security_group.new_tech_instances.id, 9000]
-    "Approved Premises Tracker API" = [aws_security_group.aptracker_api.id, 8080]
-    "GDPR API"                      = [aws_security_group.gdpr_api.id, 8080]
-    "GDPR UI"                       = [aws_security_group.gdpr_ui.id, 80]
-    "Merge API"                     = [aws_security_group.merge_api.id, 8080]
-    "Merge UI"                      = [aws_security_group.merge_ui.id, 80]
+    "User Management" = [aws_security_group.umt_instances.id, 8080]
+    "New Tech Web"    = [aws_security_group.new_tech_instances.id, 9000]
+    "GDPR API"        = [aws_security_group.gdpr_api.id, 8080]
+    "GDPR UI"         = [aws_security_group.gdpr_ui.id, 80]
+    "Merge API"       = [aws_security_group.merge_api.id, 8080]
+    "Merge UI"        = [aws_security_group.merge_ui.id, 80]
   }
   security_group_id        = aws_security_group.weblogic_ndelius_lb.id
   type                     = "egress"
