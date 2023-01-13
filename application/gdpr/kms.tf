b2bd53829ea53c6009ec0bd4bc189b3994f1d099
bill-buchan <bill.buchan@digital.justice.gov.uk>
2023-01-13 14:58:41 +0000

    Add Key for GPRS Encryption

---
+++ application/gdpr/kms.tf
@@ -0,0 +1,67 @@
+module "kms_custom_policy" {
+  source                  = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git//modules//kms_custom_policy?ref=terraform-0.12"
+  kms_key_name            = local.common_name
+  policy                  = data.aws_iam_policy_document.gdpr_rds_kms_policy_document.json
+}
+
+data "aws_iam_policy_document" "gdpr_rds_kms_policy_document" {
+ statement {
+    sid = "Enable IAM User Permissions"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:ssm:${var.region}:${data.aws_caller_identity.current.account_id}:root"]
+    }
+    actions = ["kms:*"]
+    resources = ["*"]
+  }
+ statement {
+    sid = "Allow access for Key Administrators"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:ssm:${var.region}:${data.aws_caller_identity.current.account_id}:root",
+                     "arn:aws:ssm:${var.region}:${data.aws_caller_identity.current.account_id}:role/${var.short_environment_name}-server-provison-ec2-role"]
+    }
+    actions = ["kms:Create*","kms:Describe*","kms:Enable*","kms:List*","kms:Put*","kms:Update*","kms:Revoke*",
+               "kms:Disable*","kms:Get*","kms:Delete*","kms:TagResource","kms:UntagResource",
+               "kms:ScheduleKeyDeletion","kms:CancelKeyDeletion"]
+    resources = ["*"]
+  }
+ statement {
+    sid = "Allow use of the key"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = concat(["arn:aws:ssm:${var.region}:${data.aws_caller_identity.current.account_id}:role/admin",
+                     "arn:aws:ssm:${var.region}:${data.aws_caller_identity.current.account_id}:role/${var.short_environment_name}-server-provison-ec2-role"],
+                     var.environment_name == "delius-test" ? ["arn:aws:ssm:${var.region}:${var.aws_account_ids["delius-core-non-prod"]}:role/dlc-dev-server-provision-ec2-role"]:[],
+                     var.environment_name == "delius-prod" ? ["arn:aws:ssm:${var.region}:${var.aws_account_ids["hmpps-delius-pre-prod"]}:role/del-pre-prod-server-provision-ec2-role"]:[],
+                     var.environment_name == "delius-prod" ? ["arn:aws:ssm:${var.region}:${var.aws_account_ids["hmpps-delius-stage"]}:role/del-stage-server-provision-ec2-role"]:[])
+    }
+    actions = ["kms:Encrypt","kms:Decrypt","kms:ReEncrypt*","kms:GenerateDataKey*","kms:DescribeKey"]
+    resources = ["*"]
+  }
+  statement {
+    sid = "Allow attachment of persistent resources"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = concat(["arn:aws:ssm:${var.region}:${data.aws_caller_identity.current.account_id}:role/admin",
+                     "arn:aws:ssm:${var.region}:${data.aws_caller_identity.current.account_id}:role/${var.short_environment_name}-server-provison-ec2-role"],
+                     var.environment_name == "delius-test" ? ["arn:aws:ssm:${var.region}:${var.aws_account_ids["delius-core-non-prod"]}:role/dlc-dev-server-provision-ec2-role"]:[],
+                     var.environment_name == "delius-prod" ? ["arn:aws:ssm:${var.region}:${var.aws_account_ids["hmpps-delius-pre-prod"]}:role/del-pre-prod-server-provision-ec2-role"]:[],
+                     var.environment_name == "delius-prod" ? ["arn:aws:ssm:${var.region}:${var.aws_account_ids["hmpps-delius-stage"]}:role/del-stage-server-provision-ec2-role"]:[])
+    }
+    actions = ["kms:CreateGrants","kms:ListGrants","kms:RevokeGrant"]
+    resources = ["*"]
+    condition = {
+         test = "Bool"
+         variable = "kms:GrantIsForAWSResource"
+         value = true
+    }
+  } 
+
+
+
+}
--- application/gdpr/locals.tf
+++ application/gdpr/locals.tf
@@ -3,5 +3,6 @@ locals {
   api_name   = "delius-gdpr-api"
   ui_name    = "delius-gdpr-ui"
   app_config = merge(var.default_gdpr_config, var.gdpr_config)
+  common_name = "${var.environment_identifier}-${local.app_name}"
 }
 
