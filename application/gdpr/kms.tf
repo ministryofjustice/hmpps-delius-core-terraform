c8e034349056cb08c4727be2dc48d5e4380f4e52
bill-buchan <bill.buchan@digital.justice.gov.uk>
2023-01-17 18:47:43 +0000

    Refer to policy content

--- application/gdpr/kms.tf
+++ application/gdpr/kms.tf
@@ -6,7 +6,7 @@
 module "kms_custom_policy" {
   source                  = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git//modules//kms_custom_policy?ref=terraform-0.12"
   kms_key_name            = local.common_name
-  policy                  = module.rds_kms_policy_document.aws_iam_policy_document.rds_kms_policy_document.json
+  policy                  = module.rds_kms_policy_document.aws_iam_policy_document_content.json
   tags                    = var.tags
 }
 
