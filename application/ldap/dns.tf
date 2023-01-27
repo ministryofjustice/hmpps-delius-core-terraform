2f597b96a6fbb74d8f1f6fb43d3558b3b0afc3ad
adrianweetman <adrian.weetman@digital.justice.gov.uk>
2023-01-27 15:59:42 +0000

    Allow comms from CP to ldap and create public CNAME

--- application/ldap/dns.tf
+++ application/ldap/dns.tf
@@ -1,5 +1,15 @@
+# Create record in private hosted zone
 resource "aws_route53_record" "ldap_elb_private" {
   zone_id = data.terraform_remote_state.vpc.outputs.private_zone_id
+  name    = "ldap"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_elb.lb.dns_name]
+}
+
+# Create record in public hosted zone, i.e. useful for name resolution between accounts connected through TGW
+resource "aws_route53_record" "ldap_elb_public" {
+  zone_id = data.terraform_remote_state.vpc.outputs.public_zone_id
   name    = "ldap"
   type    = "CNAME"
   ttl     = "300"
