fcc321702a0bdc03774a6f6e71752df97fddfc41
Marcus Aspin <maspin@unilink.com>
2024-01-11 13:19:15 +0000

    DST-14842 Remove Elasticsearch domain

--- elasticsearch/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  contact_search_name       = "contact-search"
-  contact_search_short_name = "contact-es" # Elasticsearch domain name is limited to 28 characters
-  contact_search_config     = merge(var.default_contact_search_config, var.contact_search_config)
-}
