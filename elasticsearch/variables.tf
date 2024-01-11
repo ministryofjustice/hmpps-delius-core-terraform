b94b1828c85daaec434e9580adc4d3739bcdf2a2
Marcus Aspin <maspin@unilink.com>
2024-01-11 14:07:58 +0000

    DST-14842 Remove Elasticsearch domain (#641)

--- elasticsearch/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "remote_state_bucket_name" {}
-variable "region" {}
-variable "environment_name" {}
-variable "short_environment_name" {}
-variable "project_name" {}
-variable "default_contact_search_config" {}
-variable "contact_search_config" {}
-variable "tags" { type = map(string) }
