95d8595c50638a2fe05b6449f7b4f55c43614907
George Taylor <george.taylor@digital.justice.gov.uk>
2024-02-13 17:19:38 +0000

    Migrate LDAP in test 🎉  (#644)
    * Update locals.tf
    
    * Update dns.tf
    
    * envs dns
    
    * Update dns.tf
    
    * Update dns.tf

--- application/ldap/locals.tf
+++ application/ldap/locals.tf
@@ -2,6 +2,11 @@ locals {
   # Override default values
   ldap_config = merge(var.default_ldap_config, var.ldap_config)
 
-  migrated_envs = ["delius-mis-dev"]
+  migrated_envs = ["delius-mis-dev", "delius-test"]
+
+  migration_nlb_dns_name = {
+    "delius-mis-dev" = "ldap-dev-nlb-7667124ead573c5a.elb.eu-west-2.amazonaws.com"
+    "delius-test" = "ldap-test-nlb-a54f13004f5f847d.elb.eu-west-2.amazonaws.com"
+  }
 }
 
