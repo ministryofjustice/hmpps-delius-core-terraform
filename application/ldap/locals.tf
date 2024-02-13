8dd1c0062585f1741db97dc22c72bb42dedfb8ea
George Taylor <george.taylor@digital.justice.gov.uk>
2024-02-13 16:35:39 +0000

    Update locals.tf

--- application/ldap/locals.tf
+++ application/ldap/locals.tf
@@ -2,6 +2,6 @@ locals {
   # Override default values
   ldap_config = merge(var.default_ldap_config, var.ldap_config)
 
-  migrated_envs = ["delius-mis-dev"]
+  migrated_envs = ["delius-mis-dev", "delius-test"]
 }
 
