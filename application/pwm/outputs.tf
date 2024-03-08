7f9af2a5fea983dbc8593917709da4f7c3e0004a
George Taylor <george.taylor@digital.justice.gov.uk>
2024-03-08 14:34:08 +0000

    dashboards

--- application/pwm/outputs.tf
+++ application/pwm/outputs.tf
@@ -21,3 +21,7 @@ output "alb" {
 output "target_group" {
   value = contains(local.migrated_envs, var.environment_name) ? null : module.service.primary_target_group
 }
+
+output "migrated" {
+  value = contains(local.migrated_envs, var.environment_name) ? true : false
+}
--- dashboards/cloudwatch.tf
+++ dashboards/cloudwatch.tf
@@ -11,10 +11,11 @@ resource "aws_cloudwatch_dashboard" "delius_service_health" {
     task_definitions = {
       weblogic-app = data.terraform_remote_state.ndelius.outputs.service["task_definition_family"]
     }
+
     load_balancers = {
       weblogic-app = data.terraform_remote_state.ndelius.outputs.alb["arn_suffix"]
       weblogic-eis = data.terraform_remote_state.interface.outputs.alb["arn_suffix"]
-      pwm          = data.terraform_remote_state.pwm.outputs != null ? data.terraform_remote_state.pwm.outputs.alb["arn_suffix"] : null
+      pwm          = data.terraform_remote_state.pwm.outputs.migrated ? null : data.terraform_remote_state.pwm.outputs.alb["arn_suffix"]
       community-api = [
         data.terraform_remote_state.community_api.outputs.alb["arn_suffix"],
         data.terraform_remote_state.community_api.outputs.public_alb["arn_suffix"],
@@ -26,7 +27,7 @@ resource "aws_cloudwatch_dashboard" "delius_service_health" {
       weblogic-eis = data.terraform_remote_state.interface.outputs.target_group["arn_suffix"]
       new-tech     = data.terraform_remote_state.new_tech.outputs.target_group["arn_suffix"]
       umt          = data.terraform_remote_state.umt.outputs.target_group["arn_suffix"]
-      pwm          = data.terraform_remote_state.pwm.outputs.target_group["arn_suffix"]
+      pwm          = data.terraform_remote_state.pwm.outputs.migrated ? null : data.terraform_remote_state.pwm.outputs.target_group["arn_suffix"]
     }
   })
 }
