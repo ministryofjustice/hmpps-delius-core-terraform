4b41fbe62cf408ce18c88656d844e67b34156fdc
Prem Basumatary <prem.basumatary@digital.justice.gov.uk>
2024-11-14 13:12:14 +0000

    TM-1 ldap cutover in stage

--- application/ldap/locals.tf
+++ application/ldap/locals.tf
@@ -2,11 +2,12 @@ locals {
   # Override default values
   ldap_config = merge(var.default_ldap_config, var.ldap_config)
 
-  migrated_envs = ["delius-mis-dev", "delius-test"]
+  migrated_envs = ["delius-mis-dev", "delius-test", "delius-stage"]
 
   migration_nlb_dns_name = {
     "delius-mis-dev" = "ldap.dev.delius-core.hmpps-development.modernisation-platform.service.justice.gov.uk"
-    "delius-test" = "ldap.test.delius-core.hmpps-test.modernisation-platform.service.justice.gov.uk"
+    "delius-test"    = "ldap.test.delius-core.hmpps-test.modernisation-platform.service.justice.gov.uk"
+    "delius-stage"   = "ldap.stage.delius-core.hmpps-preproduction.modernisation-platform.service.justice.gov.uk"
   }
 }
 
