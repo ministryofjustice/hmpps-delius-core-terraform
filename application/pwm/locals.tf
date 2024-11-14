13a3ab5d2a0b4a92bad1f0ed64d0fbcc180fb839
Andrew Moore <20435317+andrewmooreio@users.noreply.github.com>
2024-11-14 15:42:08 +0000

    chore: cutover PWM to modernisation platform

--- application/pwm/locals.tf
+++ application/pwm/locals.tf
@@ -2,11 +2,11 @@ locals {
   app_name      = "password-reset"
   short_name    = "pwm"
   app_config    = merge(var.default_pwm_config, var.pwm_config)
-  migrated_envs = ["delius-mis-dev", "delius-test"]
+  migrated_envs = ["delius-mis-dev", "delius-test", "delius-stage"]
 
   migration_url = {
     "delius-mis-dev" = "pwm.dev.delius-core.hmpps-development.modernisation-platform.service.justice.gov.uk"
     "delius-test"    = "pwm.test.delius-core.hmpps-test.modernisation-platform.service.justice.gov.uk"
+    "delius-stage"   = "pwm.stage.delius-core.hmpps-preproduction.modernisation-platform.service.justice.gov.uk"
   }
 }
-
