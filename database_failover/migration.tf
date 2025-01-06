f9096b6826b4f7eab12c0d641ec83a4097097f2a
Seb Norris <sebastian.j.norris@gmail.com>
2025-01-06 18:54:44 +0000

    add CNAME records for migration

---
+++ database_failover/migration.tf
@@ -0,0 +1,25 @@
+locals {
+  legacy_to_mp = {
+    "delius-mis-dev"  = "dev"
+    "delius-test"     = "test"
+    "delius-stage"    = "preprod"
+    "delius-pre-prod" = "preprod"
+  }
+}
+
+
+resource "aws_route53_record" "migration_internal" {
+  zone_id = data.terraform_remote_state.vpc.outputs.private_zone_id
+  name    = "delius-db-1"
+  type    = "A"
+  ttl     = "300"
+  records = ["delius-db-1.${local.legacy_to_mp[var.environment_name]}.probation.justice.gov.uk"]
+}
+
+resource "aws_route53_record" "migration_public" {
+  zone_id = data.terraform_remote_state.vpc.outputs.public_zone_id
+  name    = "delius-db-1"
+  type    = "A"
+  ttl     = "300"
+  records = ["delius-db-1.${local.legacy_to_mp[var.environment_name]}.probation.justice.gov.uk"]
+}
