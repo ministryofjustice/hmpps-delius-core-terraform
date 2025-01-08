a8d4f952e7a3f9d8c4e86804290f5bf85f066994
Seb Norris <sebastian.j.norris@gmail.com>
2025-01-08 14:29:55 +0000

    remove accidental line

--- database_failover/migration.tf
+++ database_failover/migration.tf
@@ -29,6 +29,3 @@ resource "aws_route53_record" "db1_migration_public" {
   ttl     = "300"
   records = ["delius-core-${local.legacy_to_mp_env[var.environment_name]}-db-1.delius-core.hmpps-${local.legacy_to_mp_env[var.environment_name]}.modernisation-platform.service.justice.gov.uk"]
 }
-
-
-delius-core-dev-db-1.delius-core.hmpps-development.modernisation-platform.service.justice.gov.uk
