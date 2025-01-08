68a2bf72a720139736244d1bea3cb2310bec5bda
Seb Norris <sebastian.j.norris@gmail.com>
2025-01-08 11:13:19 +0000

    use internal cname

--- database_failover/migration.tf
+++ database_failover/migration.tf
@@ -2,7 +2,7 @@ locals {
   legacy_to_mp = {
     "delius-mis-dev"  = "dev"
     "delius-test"     = "test"
-    "delius-stage"    = "preprod"
+    "delius-stage"    = "stage"
     "delius-pre-prod" = "preprod"
   }
 }
@@ -13,7 +13,7 @@ resource "aws_route53_record" "db1_migration_internal" {
   name    = "delius-db-1"
   type    = "CNAME"
   ttl     = "300"
-  records = ["delius-db-1.${local.legacy_to_mp[var.environment_name]}.probation.justice.gov.uk"]
+  records = ["delius-core-${local.legacy_to_mp[var.environment_name]}-db-1.modernisation-platform.internal"]
 }
 
 resource "aws_route53_record" "db1_migration_public" {
@@ -21,5 +21,6 @@ resource "aws_route53_record" "db1_migration_public" {
   name    = "delius-db-1"
   type    = "CNAME"
   ttl     = "300"
-  records = ["delius-db-1.${local.legacy_to_mp[var.environment_name]}.probation.justice.gov.uk"]
+  records = ["delius-core-${local.legacy_to_mp[var.environment_name]}-db-1.modernisation-platform.internal"]
 }
+
