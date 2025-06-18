79b441e713c6f109b13b09c626d6e5e02144c525
Prem Basumatary <prem.basumatary@digital.justice.gov.uk>
2025-06-18 09:16:23 +0100

    TM-1224 decommission prod ldap

--- application/ldap/locals.tf
+++ application/ldap/locals.tf
@@ -2,7 +2,7 @@ locals {
   # Override default values
   ldap_config = merge(var.default_ldap_config, var.ldap_config)
 
-  migrated_envs = ["delius-mis-dev", "delius-test", "delius-stage"]
+  migrated_envs = ["delius-mis-dev", "delius-test", "delius-stage", "delius-pre-prod", "delius-prod"]
 
   migration_nlb_dns_name = {
     "delius-mis-dev"  = "ldap.dev.delius-core.hmpps-development.modernisation-platform.service.justice.gov.uk"
